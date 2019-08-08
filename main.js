716b864b089a16c9eb389e802a8ea089840e42ae
Ryan White <mrwhiterk@gmail.com>
2019-08-08 00:52:03 -0400

    got enemies rendering in shower pattern

--- main.js
+++ main.js
@@ -1,28 +1,57 @@
 let canvas = document.querySelector('canvas');
 let ctx = canvas.getContext('2d');
 let pauseButton = document.querySelector('#pause');
+let playerSize = 20;
 let player = {
-  y: canvas.height - 25,
+  y: canvas.height - playerSize,
   x: 0,
-  height: 20,
-  width: 20
+  height: playerSize,
+  width: playerSize
 };
 let bullets = [];
-let renderId = setInterval(render, 1);
+let frameRate = 1;
+let renderId = setInterval(render, frameRate);
+let enemies = [];
 
 function render() {
   resetCanvas();
   renderPlayer();
+  setBulletMovement();
 
+  enemies.forEach((enemy, i) => {
+    if (enemies[i]) {
+      enemy.render();
+      enemies[i].y += 1;
+
+      if (enemies[i].y > canvas.height) {
+        enemies = enemies.filter(x => x !== enemies[i]);
+      }
+    }
+  });
+}
+
+setInterval(function() {
+  enemies.push(new Enemy(getRandomXcoordinate()));
+}, 500);
+
+function getRandomXcoordinate() {
+  return Math.round(Math.random() * canvas.width - player.width);
+}
+
+function setBulletMovement() {
   bullets.forEach((bullet, i) => {
     bullet.render();
     if (bullets[i]) {
       bullets[i].y -= 1;
-      if (bullets[i].y < 0) {
-        bullets = bullets.filter(x => x != bullets[i]);
-      }
+      setTopBulletBoundary(i);
     }
   });
+}
+
+function setTopBulletBoundary(i) {
+  if (bullets[i].y < 0) {
+    bullets = bullets.filter(x => x != bullets[i]);
+  }
 }
 
 function renderPlayer() {
@@ -40,7 +69,18 @@ function Bullet(x, y) {
   this.width = 10;
   this.height = 10;
   this.render = function() {
-    ctx.fillStyle = 'black';
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  };
+}
+
+function Enemy(x) {
+  this.x = x;
+  this.y = 0;
+  this.width = 20;
+  this.height = 20;
+  this.render = function() {
+    ctx.fillStyle = 'red';
     ctx.fillRect(this.x, this.y, this.width, this.height);
   };
 }
